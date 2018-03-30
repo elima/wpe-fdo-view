e386772027e0b644854fda80907794c326b0c220
Philippe Normand <philn@igalia.com>
2018-03-30 10:50:38 +0100

    Add environment variable support for fullscreen startup

--- main.c
+++ main.c
@@ -345,6 +345,12 @@ xdg_toplevel_on_configure (void *data,
 
    printf ("New XDG toplevel configuration: (%u, %u)\n", width, height);
 
+   env_var = g_getenv("WPE_FDO_VIEW_FULLSCREEN");
+   if (env_var != NULL && g_ascii_strtod(env_var, NULL) >= 1.0) {
+     zxdg_toplevel_v6_set_fullscreen(win_data.xdg_toplevel, NULL);
+     win_data.is_fullscreen = TRUE;
+   }
+
    resize_window ();
 }
 
